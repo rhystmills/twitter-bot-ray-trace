0016bf11eeab786b31639e820bd3b830e59fb4f5
Rhys Mills <rhystmills@gmail.com>
2022-12-09 14:26:18 +0000

    Tweet random image from list of urls, along with its filename

--- ray-trace.py
+++ ray-trace.py
@@ -1,5 +1,12 @@
 import tweepy
+import requests
+import os
+from io import BytesIO
+
 from decouple import config
+from PIL import Image
+
+from links import links
 
 CONSUMER_KEY = config('CONSUMER_KEY')
 CONSUMER_SECRET = config('CONSUMER_SECRET')
@@ -7,12 +14,32 @@ ACCESS_TOKEN = config('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = config('ACCESS_TOKEN_SECRET')
 BEARER_TOKEN = config('BEARER_TOKEN')
 
-client = tweepy.Client(
-   BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-)
+# For v2 auth:
+# client = tweepy.Client(
+#    BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+# )
+# client.create_tweet(text="Hello, world.")
 
-print(
+v1_auth = auth = tweepy.OAuth1UserHandler(
    CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 )
 
-client.create_tweet(text="Hello, world.")
+def image_to_byte_array(image: Image) -> bytes:
+  # BytesIO is a fake file stored in memory
+  imgByteArr = BytesIO()
+  # image.save expects a file as a argument, passing a bytes io ins
+  image.save(imgByteArr, format=image.format)
+  # Turn the BytesIO object back into a bytes object
+  imgByteArr = imgByteArr.getvalue()
+  return imgByteArr
+
+v1_api = tweepy.API(v1_auth)
+
+url = random.choice(list)
+path_end = os.path.basename(os.path.normpath(url))
+
+response = requests.get(url)
+img = Image.open(BytesIO(response.content))
+img_bytes = image_to_byte_array(img)
+
+v1_api.update_status_with_media(status=path_end, filename=path_end, file=img_bytes)
